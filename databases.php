966f889aae710abe2b3140eb46d21a80df564471
Margit <margit.pilden@gmail.com>
2014-11-18 13:49:38 +0200

    add stuff - not finished

--- databases.php
+++ databases.php
@@ -1,3 +1,14 @@
+<?php
+	$dbhost = "localhost";
+	$dbuser = "widget_cms"; 
+	$dbpass = "tupsununnu123"; 
+	$dbname = "widget_corp"; 
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	$query = "SELECT * FROM subjects;"; 
+	$result = mysqli_query($connection, $query);
+	$row = mysqli_fetch_row($result);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,7 +17,18 @@
   </head>
 
   <body>
- 
+	<?php
 	
+ <article class="page">
+  <header class="page-header">
+    <h1 class="post-title">// Uudise pealkiri</h1>
+  </header>
+  <div class="page-body">
+    // Postituse sisu
+   </div>
+</article>
+?>
   </body>
 </html>
+
+<?php mysqli_close($connection); ?>
